d0f5f189e7113c8acbc65cc7237f054850a04d18
Patrick Duffy <pduffy@ms.innovativesol.com>
2019-09-23 15:24:59 -0400

    Added Get-GroupMembership CMDLET

--- Public/Get-AccountPWExpire.ps1
+++ Public/Get-AccountPWExpire.ps1
@@ -1,18 +1,18 @@
 function Get-AccountPWExpire
 {
-    [CmdletBinding()]
-    Param
+    [cmdletbinding()]
+    param
     (
-        [Parameter(mandatory=$false,position=0,HelpMessage="If used this will export to file instead of console.")]
-        [Switch]
+        [parameter(mandatory=$false,position=0,helpmessage="If used this will export to file instead of console.")]
+        [switch]
         $Export
     )
 
-    if($export)
+    if($Export)
     {
         try 
         {
-            Get-ADUser -filter {Enabled -eq $True -and PasswordNeverExpires -eq $False} –Properties "DisplayName", "msDS-UserPasswordExpiryTimeComputed" | Select-Object -Property "Displayname",@{Name="ExpiryDate";Expression={[datetime]::FromFileTime($_."msDS-UserPasswordExpiryTimeComputed")}} | Export-CSV AccountPWExpires.csv -NoTypeInformation
+            Get-ADUser -filter {Enabled -eq $true -and PasswordNeverExpires -eq $false} –Properties "DisplayName", "msDS-UserPasswordExpiryTimeComputed" | Select-Object -Property "Displayname",@{Name="ExpiryDate";Expression={[datetime]::FromFileTime($_."msDS-UserPasswordExpiryTimeComputed")}} | Export-CSV AccountPWExpires.csv -NoTypeInformation
             Write-Output "AccountPWExpires.csv exported to current directory"
         }
         catch 
@@ -26,7 +26,7 @@ function Get-AccountPWExpire
     {
         try 
         {
-            Get-ADUser -filter {Enabled -eq $True -and PasswordNeverExpires -eq $False} –Properties "DisplayName", "msDS-UserPasswordExpiryTimeComputed" | Select-Object -Property "Displayname",@{Name="ExpiryDate";Expression={[datetime]::FromFileTime($_."msDS-UserPasswordExpiryTimeComputed")}} | Write-Output | Format-Table
+            Get-ADUser -filter {Enabled -eq $true -and PasswordNeverExpires -eq $false} –Properties "DisplayName", "msDS-UserPasswordExpiryTimeComputed" | Select-Object -Property "Displayname",@{Name="ExpiryDate";Expression={[datetime]::FromFileTime($_."msDS-UserPasswordExpiryTimeComputed")}} | Write-Output | Format-Table
         }
         catch 
         {
---
+++ Public/Get-GroupMembership.ps1
@@ -0,0 +1,47 @@
+function Get-GroupMembership
+{
+    <#
+    .SYNOPSIS
+    Find's group membership for all AD users and exports to console or CSV.
+    
+    .DESCRIPTION
+    Find's group membership for all AD users and exports to console or CSV.
+    
+    .EXAMPLE
+    Copy-GroupMembership
+    #>
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(mandatory=$false,position=0,HelpMessage="If used this will export to file instead of console.")]
+        [Switch]
+        $Export
+    )
+
+    
+    $Output = @()
+    $ADUsers = Get-ADUser -Filter *
+
+    foreach($ADUser in $ADUsers)
+    {
+        try
+        {
+            $ADUserGroup = Get-ADPrincipalGroupMembership -Identity $ADUser
+            $Properties = [ordered]@{'Username'=$ADUser.Name;'GroupMembership'=$ADUserGroup.Name}
+            $Output += New-Object -TypeName PSObject -Property $Properties
+        }
+        catch
+        {
+            
+        }
+    }
+
+    if($Export)
+    {
+        $Output | Write-Output | Export-CSV UserGroupMembership.csv
+    }
+    else
+    {
+        $Output | Write-Output | Out-GridView
+    } 
+}
